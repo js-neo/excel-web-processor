afb93309d324bb6ebf25b05e2fad46ad57f20b2f
js-neo <yuriy.jsx@gmail.com>
2024-12-21 18:39:25 +0300

    feat: update Webpack configuration and HTML file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
     entry: './src/renderer.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(process.cwd(), 'dist'),
+        publicPath: '/excel-web-processor/',
     },
     mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
 };
+
+

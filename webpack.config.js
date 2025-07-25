433acbfe67f2e037d035ccfff1ccd33b06f8197f
js-neo <jsneoweb@gmail.com>
2025-07-25 16:34:22 +0300

    feat(webpack.config.js): update config for improved build performance

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,57 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default {
     entry: './src/renderer.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(process.cwd(), 'dist'),
-        publicPath: '/excel-web-processor/',
+        publicPath: isDev ? '/' : '/excel-web-processor/',
     },
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(process.cwd(), 'dist'),
+        },
+        hot: true,
+        open: true,
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: 'index.html',
             inject: 'body',
-            scriptLoading: 'blocking',
+            scriptLoading: 'defer',
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.js$/,
